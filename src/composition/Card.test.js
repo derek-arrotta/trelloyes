24a951e55179cb3a0d67fb9314630e7cdf68edf9
Derek Arrotta <derek.arrotta@gmail.com>
2020-11-19 12:56:07 -0500

    add card test file, case 1 render without crashing

---
+++ src/composition/Card.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Card from './Card';
+import renderer from 'react-test-renderer';
+
+describe('card component', () => {
+  
+  it('renders without crashing', () =>{
+    const div = document.createElement('div');
+    ReactDOM.render(<Card />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+  
+});
